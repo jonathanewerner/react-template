4f47b9b20efd8d99c27cfbe4846f2e3a56bddff8
Jonathan Werner <jwerner@posteo.de>
2014-11-24 13:06:50 +0100

    init

---
+++ app/components/App.jsx
@@ -0,0 +1,10 @@
+var React = require('react'),
+    Hello = require('components/Hello');
+
+module.exports = React.createClass({
+    render() {
+      return (
+          <Hello />
+      );
+    }
+});
